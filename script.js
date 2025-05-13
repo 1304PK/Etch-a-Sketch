ca07f22b27d8f7e69e5a4ea02f9cd5007295648f
1304PK <pushkark2005@gmail.com>
2025-05-13 11:18:33 +0530

    Add event Listeners to all the widgets tab buttons

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 const container = document.querySelector('.container')
+const setGridButton = document.getElementById('set-grid-size')
+let gridSize = 0
+const clearCanvas = document.getElementById('clear-canvas')
+const colorValue = document.getElementById('color-selector')
+const colorMethod = document.getElementById('set-color-method')
 
-for (let i = 1; i <= 10; i++) {
-    const horizontalSketchDiv = document.createElement('div')
-    horizontalSketchDiv.classList.add('horizontalSketches')
-    for (let j = 1; j <=10; j++){
-        const verticalSketchDiv = document.createElement('div')
-        verticalSketchDiv.classList.add('verticalSketches')
-        horizontalSketchDiv.appendChild(verticalSketchDiv)
+colorMethod.addEventListener('click', () => {
+    if (colorMethod.textContent === 'Use Random Color'){
+        colorMethod.textContent = 'Use Black Color'
     }
-    container.appendChild(horizontalSketchDiv)
-}
+    else if (colorMethod.textContent === 'Use Black Color'){
+        colorMethod.textContent = 'Use Random Color'
+    }
+})
+
+setGridButton.addEventListener('click', function() {
+    gridSize = prompt('Enter Grid Size(maximum 100)')
+    container.innerHTML = ''
+    for (let i = 1; i <= gridSize; i++) {
+        const horizontalSketchDiv = document.createElement('div')
+        horizontalSketchDiv.classList.add('horizontalSketches')
+        for (let j = 1; j <=gridSize; j++){
+            const verticalSketchDiv = document.createElement('div')
+            verticalSketchDiv.classList.add('verticalSketches')
+            verticalSketchDiv.addEventListener('mouseover', () => {verticalSketchDiv.setAttribute('style', `background-color: ${colorValue.value}`)})
+            horizontalSketchDiv.append(verticalSketchDiv)
+            clearCanvas.addEventListener('click', () => {
+                verticalSketchDiv.setAttribute('style', 'background-color: white')
+            })
+        }
+        
+        
+        container.append(horizontalSketchDiv)
+    }
+    
+    
+})
+
+
+
