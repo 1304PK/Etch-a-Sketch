55af7d910d505efaefa81fdd91c1664bca3ac759
1304PK <pushkark2005@gmail.com>
2025-05-13 11:33:09 +0530

    Add transition to background color

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const clearCanvas = document.getElementById('clear-canvas')
 const colorValue = document.getElementById('color-selector')
 const colorMethod = document.getElementById('set-color-method')
 
+console.log(colorMethod.offsetWidth)
+
 colorMethod.addEventListener('click', () => {
     if (colorMethod.textContent === 'Use Random Color'){
         colorMethod.textContent = 'Use Black Color'
@@ -23,7 +25,9 @@ setGridButton.addEventListener('click', function() {
         for (let j = 1; j <=gridSize; j++){
             const verticalSketchDiv = document.createElement('div')
             verticalSketchDiv.classList.add('verticalSketches')
-            verticalSketchDiv.addEventListener('mouseover', () => {verticalSketchDiv.setAttribute('style', `background-color: ${colorValue.value}`)})
+            verticalSketchDiv.addEventListener('mouseover', () => {verticalSketchDiv.setAttribute('style', `transition: background-color 230ms;
+                background-color: ${colorValue.value}`)
+            })
             horizontalSketchDiv.append(verticalSketchDiv)
             clearCanvas.addEventListener('click', () => {
                 verticalSketchDiv.setAttribute('style', 'background-color: white')
