9aefa34c2481173eb4348b2a0d56315179e076e4
1304PK <pushkark2005@gmail.com>
2025-06-04 23:41:18 +0530

    random color feature

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 const container = document.querySelector('.container')
 const setGridButton = document.getElementById('set-grid-size')
-let gridSize = 0
 const clearCanvas = document.getElementById('clear-canvas')
 const colorValue = document.getElementById('color-selector')
-const colorMethod = document.getElementById('set-color-method')
+const colorMethod = document.getElementById('custom-color')
 
 colorMethod.addEventListener('click', () => {
-    if (colorMethod.textContent === 'Use Random Color'){
-        colorMethod.textContent = 'Use Custom Color'
-    }
-    else if (colorMethod.textContent === 'Use Custom Color'){
+    if (colorMethod.textContent === 'Use Custom Color') {
         colorMethod.textContent = 'Use Random Color'
+    }
+    else if (colorMethod.textContent === 'Use Random Color') {
+        colorMethod.textContent = 'Use Custom Color'
     }
 })
 
-setGridButton.addEventListener('click', function() {
-    gridSize = prompt('Enter Grid Size(maximum 100)')
-    console.log(gridSize)
-    
-    if (gridSize > 0 && gridSize<=100){
+setGridButton.addEventListener('click', function () {
+    const gridSize = prompt('Enter Grid Size(maximum 100)')
+
+    if (gridSize > 0 && gridSize <= 100) {
         container.innerHTML = ''
         for (let i = 1; i <= gridSize; i++) {
             const horizontalSketchDiv = document.createElement('div')
             horizontalSketchDiv.classList.add('horizontalSketches')
-            for (let j = 1; j <=gridSize; j++){
+            for (let j = 1; j <= gridSize; j++) {
                 const verticalSketchDiv = document.createElement('div')
                 verticalSketchDiv.classList.add('verticalSketches')
-                verticalSketchDiv.addEventListener('mouseover', () => {verticalSketchDiv.setAttribute('style', `transition: background-color 230ms;
-                    background-color: ${colorValue.value}`)
+                verticalSketchDiv.addEventListener('mouseover', () => {
+
+                    let color = ''
+                    if (colorMethod.textContent === 'Use Custom Color') {
+                        color = `hsl(${Math.random() * 360}, 100%, 50%)`
+                    }
+                    else {
+                        color = colorValue.value
+                    }
+                    verticalSketchDiv.setAttribute('style', `transition: background-color 230ms;
+                    background-color: ${color}`)
                 })
                 horizontalSketchDiv.append(verticalSketchDiv)
                 clearCanvas.addEventListener('click', () => {
                     verticalSketchDiv.setAttribute('style', 'background-color: white')
                 })
             }
-            
+
             container.append(horizontalSketchDiv)
         }
-    } 
-    else{
+    }
+    else {
         alert('Enter a valid number between 1-100')
     }
-
-    
-    
-    
 })
-
-
-
